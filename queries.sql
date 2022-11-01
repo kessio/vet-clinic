6a33128052614d372ea85f830dbb34c69813b117
unknown <sharonkessio6@gmail.com>
2022-11-01 13:10:09 +0300

    query date of birth between 2016 and 2019

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals where name LIKE '%mon';
+
+SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31'
