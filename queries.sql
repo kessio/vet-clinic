cca6a930252fa0039722c549ec1076fa2920ba60
unknown <sharonkessio6@gmail.com>
2022-11-01 13:12:21 +0300

    chnage query to only select name for date of birth

--- queries.sql
+++ queries.sql
@@ -2,4 +2,4 @@
 
 SELECT * FROM animals where name LIKE '%mon';
 
-SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31'
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31'
