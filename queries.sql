8d9ef0a88b572d10d933c560c694791099c64c3e
unknown <sharonkessio6@gmail.com>
2022-11-01 13:15:34 +0300

    query natired animals and escape attempt less than 3

--- queries.sql
+++ queries.sql
@@ -3,3 +3,5 @@
 SELECT * FROM animals where name LIKE '%mon';
 
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31'
+
+SELECT name FROM animals WHERE neutered=true AND escape_attempts < 3
