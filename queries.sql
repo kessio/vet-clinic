9fc826f454e18f0a92a10c363f47fad62928b2bf
unknown <sharonkessio6@gmail.com>
2022-11-03 11:49:06 +0300

    added more animals to table and query

--- queries.sql
+++ queries.sql
@@ -17,3 +17,33 @@ SELECT * FROM animals WHERE name != 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
+/*
+Inside a transaction:
+Update the animals table by setting the species column to unspecified. Verify that change was made.
+Then roll back the change and verify that the species columns went back to the state before the transaction.
+*/
+
+BEGIN TRANSACTION;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+
+ROLLBACK;
+SELECT * FROM animals;
+
+/*
+Inside a transaction:
+Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+Commit the transaction.
+Verify that change was made and persists after commit.
+*/
+
+BEGIN TRANSACTION;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+SELECT * FROM animals;
+
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT * FROM animals;
+COMMIT;
+SELECT * FROM animals;
+
--- schema.sql
+++ schema.sql
@@ -1,2 +1,10 @@
 /* Database schema to keep the structure of entire database. */
-
+CREATE TABLE animals (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name varchar(100),
+    date_of_birth date,
+    escape_attempts int,
+    neutered boolean,
+    weight_kg decimal
+);
+alter table animals add column species varchar;
