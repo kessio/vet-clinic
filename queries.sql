e12d6b87acb192c4eec278f684c7ff0859ce0699
unknown <sharonkessio6@gmail.com>
2022-11-03 12:41:53 +0300

    Escape attempts

--- queries.sql
+++ queries.sql
@@ -91,3 +91,6 @@ SELECT neutered, SUM(escape_attempts) AS MOST_ESCAPE_ATTEMPTS_GROUP FROM animals
 
 -- What is the minimum and maximum weight of each type of animal?
 SELECT species, MIN(weight_kg) AS MIN_WEIGHT, MAX(weight_kg) AS MAX_WEIGHT FROM animals GROUP BY species;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, AVG(escape_attempts) AS AVG_ESCAPE_ATTEMPTS FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species ;
