93c3a6da948a8671448272d9b5517c92768cd0a2
Farida-Hamid <faridaaddala@gmail.com>
2022-11-08 17:52:04 +0300

    Add medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,3 +3,11 @@ CREATE TABLE patients (
     name varchar(100),
     date_of_birth date
 );
+
+CREATE TABLE medical_histories (
+    id SERIAL PRIMARY KEY,
+    admitted_at TIMESTAMP,
+    patients_id INT,
+    status varchar(255),
+    FOREIGN KEY(patients_id) REFERENCES patients(id)
+ );
